0ae58dae7e93ee0bde29e8f81ae7760569eff24e
Will <48477001+SmoothieDevs@users.noreply.github.com>
2024-06-17 16:02:27 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const fs = require('fs');
+const fs = require('fs'); 
+
+function getHtmlPlugins(sourcePath) {
+    let files = fs.readdirSync(sourcePath, { withFileTypes: true });
+    let htmlFiles = [];
+
+    files.forEach(file => {
+        if (file.isDirectory()) {
+            htmlFiles = [...htmlFiles, ...getHtmlPlugins(path.join(sourcePath, file.name))];
+        } else if (file.name.endsWith('.html') && file.name !== 'index.html' && file.name !== '404.html') {
+            htmlFiles.push(new HtmlWebpackPlugin({
+                template: path.join(sourcePath, file.name),
+                filename: path.join('views', file.name),
+            }));
+        }
+    });
+
+    return htmlFiles;
+}
 
 module.exports = {
-    entry: './src/js/main.js',
+    entry: "./src/js/main.js",
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -26,6 +44,10 @@ module.exports = {
             template: './src/index.html',
             filename: 'index.html',
         }),
+        new HtmlWebpackPlugin({
+            template: './src/404.html',
+            filename: '404.html',
+        }),
         ...getHtmlPlugins(path.resolve(__dirname, 'src')),
         new MiniCssExtractPlugin({
             filename: '[name].css',
@@ -37,19 +59,9 @@ module.exports = {
             directory: path.join(__dirname, 'src'),
         },
         compress: true,
+        host: "0.0.0.0",
         port: 8080,
         open: true,
         hot: true,
     },
 };
-
-function getHtmlPlugins(sourcePath) {
-    return fs.readdirSync(sourcePath)
-        .filter(file => file.endsWith('.html') && file !== 'index.html')
-        .map(file => {
-            return new HtmlWebpackPlugin({
-                template: path.join(sourcePath, file),
-                filename: file,
-            });
-        });
-}
