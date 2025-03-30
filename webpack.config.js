0f71ad662b419581f25a185c1cc4f242cf5101be
Liam <smoothiedeveloppeur@gmail.com>
2025-03-30 20:14:56 +0200

    2025 update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const fs = require('fs'); 
+const fs = require('fs');
 
 function getHtmlPlugins(sourcePath) {
     let files = fs.readdirSync(sourcePath, { withFileTypes: true });
